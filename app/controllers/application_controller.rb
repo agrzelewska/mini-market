088287776e9e540e1439993d758bef485dce8713
agrzelewska <agatagrzelewska@gmail.com>
2018-06-02 01:25:09 +0200

    Login, logout, signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+	helper_method :current_customer
+
+	def current_customer
+		@current_customer ||= Customer.find(session[:customer_id]) if session[:customer_id]
+	end
+
 end
